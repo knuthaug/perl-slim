c291be8833c4ecfd5b867b121e6ec265a0e51d6a
Knut Haugen <knuthaug+github@gmail.com>
2010-01-19 20:29:35 +0100

    Added basic networking support to get serialized data over the wire

--- lib/Slim/SocketHandler.pm
+++ lib/Slim/SocketHandler.pm
@@ -1,15 +1,33 @@
 package Slim::SocketHandler;
 
 use Moose;
+use threads;
+use threads::shared;
 use namespace::autoclean;
 use Error;
 use Time::HiRes qw(alarm);
+use IO::Socket;
 
 
 has 'port' => (
                is => 'rw', 
                default => 12345,
                isa => 'Int',
+              );
+
+has 'action' => (
+               is => 'rw', 
+               isa => 'CodeRef',
+              );
+
+has 'listener_thread' => (
+               is => 'rw', 
+               isa => 'Object',
+              );
+
+has 'socket' => (
+               is => 'rw', 
+               isa => 'IO::Socket',
               );
 
 
@@ -29,17 +47,72 @@ Knut Haugen <knuthaug@gmail.com>
 
 =over 4
 
-=item run ()
+=item handle ()
 
-Run a server on the port given in the constructor
+Run a socket listening on the port given in the constructor
 
 =cut
 
+sub handle { 
+    my($self, $action) = @_;
+    
+    $self->action($action);
+    $self->socket($self->open_socket() ); 
+    $self->listener_thread(threads->create( 'listen', $self));
+}
+
+
+sub close_all {
+    my($self) = @_;
+    
+    shutdown($self->socket, 2);
+    foreach my $thread (threads->list()) {
+        $thread->join();
+    }
+}
 
 
 =back
 
 =cut
+
+
+
+sub listen {
+    my($self) = @_;
+
+    while ( my $connection = $self->socket()->accept() ) {
+        threads->create('handle_connection', $self, $connection);
+    }
+    
+}
+
+sub handle_connection {
+    my($self, $connection) = @_;
+    my $return = $self->action->($connection);
+
+    #while (<$connection>) {
+    #    print STDERR "got: " . $_;
+    #}
+    close($connection);
+    return $return;
+    
+}
+
+sub open_socket { 
+    my($self) = @_;
+    my $socket = IO::Socket::INET->new(LocalPort => $self->port,
+                                       LocalHost => 'localhost',
+                                       Reuse     => 1,
+                                       Listen    => 1,
+                                       Type      => SOCK_STREAM,
+                                       Proto     => 'tcp', 
+                                      Blocking => 1)
+      or confess "Couldn't create a tcp server on port " . $self->port() . " : $@\n";
+    return $socket;
+    
+}
+
 
 
 no Moose;
--- t/lib/Test/Slim/Networking.pm
+++ t/lib/Test/Slim/Networking.pm
@@ -1,37 +1,69 @@
 package Test::Slim::Networking;
 
-use base qw(Test::Class);
-use Test::More;
-#use Test::Exception;
 use strict;
 use warnings;
+use base qw(Test::Class);
 
-use Slim::Server;
+use Test::More;
+#use Test::Exception;
+use Time::HiRes qw(sleep);
+use Carp;
+use IO::Socket;
+
+#use Slim::Server;
 use Slim::SocketHandler;
+use threads;
+use threads::shared;
 
-my $server;
+#my $server;
 my $socket_handler;
-my $connections;
+my $connections :shared;
 
 my $port = 12345;
 
 sub setup_fixture : Test(setup) {
-    $server = new Slim::Server({port =>$port});
-    $socket_handler = new Slim::SocketHandler({port => $port});
+    $socket_handler = new Slim::SocketHandler({ port => $port });
     $connections = 0;
 }
 
-sub can_run_the_server : Test(1) {
-    is(1, $server->run, "Run returns (fake) 1")
-}
+#sub can_run_the_server : Test(1) {
+    #is(1, $server->run, "Run returns (fake) 1")
+#}
 
 
 #socket tests
 
-sub can_handle_one_connection: Test(1) {
-    is(1, $connections, "1 alive connection after close");
+sub can_handle_one_connection : Test(1) {
+    $socket_handler->handle( sub { $connections += 1;  } );
+    connect_socket();
+    $socket_handler->close_all;
+
+    is($connections, 1, "1 connection after close");
 }
 
+
+sub can_handle_many_connections : Test(1) {
+    $socket_handler->handle( sub { $connections += 1;  } );
+
+    foreach (1..10) {
+        connect_socket();
+    }
+
+    $socket_handler->close_all;
+
+    is($connections, 10, "10 connections after close");
+    
+}
+
+sub connect_socket {
+
+    my $socket = IO::Socket::INET->new(PeerAddr => 'localhost',
+                                       PeerPort => $port,
+                                       Proto    => "tcp")
+      or confess "Couldn't connect to $port : $@\n";
+    sleep(0.1);
+    close($socket);
+}
 
 
 1;
