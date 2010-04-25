5ba115811bb679bc6091801b255e14a74a4b1e53
Knut Haugen <knuthaug+github@gmail.com>
2010-04-25 12:43:03 +0200

    transformations for method names

--- lib/Slim/Statement.pm
+++ lib/Slim/Statement.pm
@@ -17,9 +17,15 @@ Knut Haugen <knuthaug@gmail.com>
 
 sub slim_to_perl_class {
     my($self, $class_string) = @_;
-    my @parts = split(/\.|\:\:/, $class_string);
-    return join "::", map { ucfirst $_ } @parts;
+    my @parts = split /\.|\:\:/, $class_string;
+    join "::", map { ucfirst $_ } @parts;
  
+}
+
+sub slim_to_perl_method {
+    my($self, $method_string) = @_;
+    $method_string =~ s|([A-Z])|"_" . lc($1)|eg;
+    $method_string;
 }
 
 no Moose;
--- t/lib/Test/Slim/StatementTransformations.pm
+++ t/lib/Test/Slim/StatementTransformations.pm
@@ -12,10 +12,17 @@ sub setup_fixture : Test(setup) {
     $statement = new Slim::Statement();
 }
 
-sub can_translate_slim_classes_to_perl_class_names : Test(2) {
+sub must_translate_slim_classes_to_perl_class_names : Test(2) {
     is( $statement->slim_to_perl_class("package.Class"), "Package::Class", "Transforms . to ::");
     is( $statement->slim_to_perl_class("package.subPackage::class"), 
         "Package::SubPackage::Class", "Transforms . to :: and uppercases");
+}
+
+sub must_translate_slim_method_names_to_perl : Test(2) {
+    is( $statement->slim_to_perl_method("myMethod"), "my_method", 
+        "transforms case and Camel case to underscore ad lowercase ");
+    is( $statement->slim_to_perl_method("myMethodTwo"), "my_method_two", 
+        "transforms case and Camel case to underscore ad lowercase. Multiples ");
 }
 
 
